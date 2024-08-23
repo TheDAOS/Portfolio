4b8af8e1680b9ac480240d21c7ca5379181ac5a4
theDAOS <sanjusanthosh46@gmail.com>
2024-08-23 12:37:24 +0530

    clean up

--- my-terminal.js
+++ my-terminal.js
@@ -1,90 +1,10 @@
-const root = '~';
-let cwd = root
-
 const commands = {
     help() {
         term.echo(`List of available commands: ${help}`);
-        // term.html(`List of available commands: ${help}`);
     },
     echo(...args) {
         term.echo(args.join(' '));
-    },
-    cd(dir = null) {
-        if (dir === null || (dir === '..' && cwd !== root)) {
-            cwd = root;
-        } else if (dir.startsWith('~/') && directories.includes(dir.substring(2))) {
-            cwd = dir;
-        } else if (directories.includes(dir)) {
-            cwd = root + '/' + dir;
-        } else {
-            this.error('Wrong directory');
-        }
     }
-};
-
-const directories = {
-    education: [
-        '',
-        '<white>education</white>',
-
-        '* <a href="https://en.wikipedia.org/wiki/Kielce_University_of_Technology">Kielce University of Technology</a> <yellow>"Computer Science"</yellow> 2002-2007 / 2011-2014',
-        '* <a href="https://pl.wikipedia.org/wiki/Szko%C5%82a_policealna">Post-secondary</a> Electronic School <yellow>"Computer Systems"</yellow> 2000-2002',
-        '* Electronic <a href="https://en.wikipedia.org/wiki/Technikum_(Polish_education)">Technikum</a> with major <yellow>"RTV"</yellow> 1995-2000',
-        ''
-    ],
-    projects: [
-        '',
-        '<white>Open Source projects</white>',
-        [
-            ['jQuery Terminal',
-             'https://terminal.jcubic.pl',
-             'library that adds terminal interface to websites'
-            ],
-            ['LIPS Scheme',
-             'https://lips.js.org',
-             'Scheme implementation in JavaScript'
-            ],
-            ['Sysend.js',
-             'https://jcu.bi/sysend',
-             'Communication between open tabs'
-            ],
-            ['Wayne',
-             'https://jcu.bi/wayne',
-             'Pure in browser HTTP requests'
-            ],
-        ].map(([name, url, description = '']) => {
-            return `* <a href="${url}">${name}</a> &mdash; <white>${description}</white>`;
-        }),
-        ''
-    ].flat(),
-    skills: [
-        '',
-        '<white>languages</white>',
-
-        [
-            'JavaScript',
-            'TypeScript',
-            'Python',
-            'SQL',
-            'PHP',
-            'Bash'
-        ].map(lang => `* <yellow>${lang}</yellow>`),
-        '',
-        '<white>libraries</white>',
-        [
-            'React.js',
-            'Redux',
-            'Jest',
-        ].map(lib => `* <green>${lib}</green>`),
-        '',
-        '<white>tools</white>',
-        [
-            'Docker',
-            'git',
-            'GNU/Linux'
-        ].map(lib => `* <blue>${lib}</blue>`),
-        ''
-    ].flat()
 };
 
 
@@ -120,8 +40,6 @@ function ready() {
     term.echo(() => rainbow(render('Terminal Portfolio'), seed))
         .echo('Welcome to my Terminal Portfolio\n').resume();
 }
- 
-
 
 const font = 'Slant';
 

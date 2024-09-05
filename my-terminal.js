28d679709dbdf1d1b24a7d9d80c554db0560e28e
theDAOS <sanjusanthosh46@gmail.com>
2024-09-05 20:07:51 +0530

    testing

--- my-terminal.js
+++ my-terminal.js
@@ -1,6 +1,6 @@
 const commands = {
     help() {
-        term.echo(`List of available commands: ${help}`);
+        term.echo(`List of available commands: ${help}`, {raw: true});
     },
     echo(...args) {
         term.echo(args.join(' '));
